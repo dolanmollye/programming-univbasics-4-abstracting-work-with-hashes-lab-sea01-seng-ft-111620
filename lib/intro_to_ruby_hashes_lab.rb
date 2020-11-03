3832cd5b45ec2a98166a9a0c827d8dfe71b980a2
Molly Dolan <dolanmollye@gmail.com>
2020-11-03 05:09:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,5 @@
 def my_hash_creator(key, value)
-  my_hash = {:name => "Grace Hopper"}
-  my_hash
-  # return a hash that includes the key and value parameters passed into this method
+  {key => value}
 end
 
 def read_from_hash(hash, key)
